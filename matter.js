16a238ea87a1c50a55ee64cfd35750aa7b4bafd5
clemtre <ernest@gmail.com>
2021-12-09 16:30:34 +0100

    page daccueuil

--- matter.js
+++ matter.js
@@ -30,7 +30,7 @@ var Example = Example || {};
             height: window.innerHeight,
             // showAngleIndicator: true,
             wireframes : false,
-            background:'white',
+            background:'transparent',
             showSleeping:false
         }
     });
@@ -42,19 +42,25 @@ var Example = Example || {};
     Runner.run(runner, engine);
 
     // add bodies
-    var size = 30;
-    var w = 10;
-    var rdmAmp = 40;
+    var size = 60;
+    var w = 20;
+    var rdmAmp = window.innerWidth/5;
 
-    var stack = Composites.stack(window.innerWidth/10, -1000, 15,80,  window.innerWidth/25, 5, function(x, y) {
+    var stack = Composites.stack(0, -1000, 15,80,  window.innerWidth/25, 5, function(x, y) {
         var rdm = -rdmAmp/2+Math.random()*rdmAmp;
         var rdmA = Math.random()*Math.PI*2;
-        var partA = Bodies.rectangle(x + rdm, y, size, size / w, {render : {fillStyle: 'black'}}),
-            partB = Bodies.rectangle(x + rdm, y, size / w, size, { render: partA.render});
+        var ampLr = 2;
+        var partA = Bodies.rectangle(x + rdm, y, size * Math.random()*ampLr, size / w, {render : {fillStyle: 'black'}}),
+            partD = Bodies.rectangle(x + rdm, y, size * Math.random()*ampLr, size / w, {render : {fillStyle: 'black'}}),
+            partB = Bodies.rectangle(x + rdm, y, size / w, size * Math.random()*ampLr, { render: partA.render}),
+            partC = Bodies.rectangle(x + rdm, y, size / w, size * Math.random()*ampLr, { render: partA.render});
+
             Body.rotate(partA,rdmA);
             Body.rotate(partB,rdmA);
+            Body.rotate(partC,rdmA+Math.PI/4);
+            Body.rotate(partD,rdmA+Math.PI/4);
         return Body.create({
-            parts: [partA, partB],
+            parts: [partA, partB, partC, partD],
             friction:1,
             mass:0,
             restitution:0,
@@ -71,7 +77,9 @@ var Example = Example || {};
         // Bodies.rectangle(800, 300, 50, 600, { isStatic: true, render:{visible:true} }),
         // Bodies.rectangle(Math.random()*800, 800, 50, 600, { isStatic: true, render:{visible:true} }),
         // Bodies.rectangle(0, 300, 50, 600, { isStatic: true, render:{visible:true} }),
-        Bodies.rectangle(0, window.innerHeight, window.innerWidth*2, 50, { isStatic: true, render:{visible:false, fillStyle:'red'} })
+        Bodies.rectangle(0, window.innerHeight , window.innerWidth*2, 5, { isStatic: true, render:{visible:false, fillStyle:'red'} }),
+        Bodies.rectangle(0, 0 , 5, window.innerHeight*2, { isStatic: true, render:{visible:false, fillStyle:'red'} }),
+        Bodies.rectangle(window.innerWidth, 0 , 5, window.innerHeight*2, { isStatic: true, render:{visible:false, fillStyle:'red'} })
     ]);
 
     for (var i = 0; i < stack.bodies.length; i++) {
@@ -98,7 +106,18 @@ var Example = Example || {};
 
     // keep the mouse in sync with rendering
     render.mouse = mouse;
+    
+    function displayWindowSize(){
+        // World.clear(engine.world);
+        // Engine.clear(engine);
+        // World.clear(engine.world);
+                console.log(window.innerWidth);    
+                // render.canvas.remove();
 
+    }
+
+window.addEventListener("resize", displayWindowSize);
+// displayWindowSize();
     // fit the render viewport to the scene
     // Render.lookAt(render, {
     //     min: { x: 0, y: 0 },
